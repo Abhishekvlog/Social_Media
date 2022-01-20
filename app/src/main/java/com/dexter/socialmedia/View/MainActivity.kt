d6666af702316149a85d00227600cb037a8a407e
abhishekvlog <abhishek.kumar.kps25@gmail.com>
2022-01-20 12:03:06 +0530

    Initial commit

---
+++ app/src/main/java/com/dexter/socialmedia/View/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dexter.socialmedia.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dexter.socialmedia.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
